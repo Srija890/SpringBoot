663797b31f248c4556957834b2eea2ded15f50f6
srija <srijak890@gmail.com>
2019-07-25 10:08:01 +0530

    Lombok

--- src/main/java/com/stackroute/domain/Track.java
+++ src/main/java/com/stackroute/domain/Track.java
@@ -1,66 +1,23 @@
-
 package com.stackroute.domain;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-@ApiModel(description = "Track info...")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track
 {
     // Declaration of fields in table
     @Id
     int id;
-    @ApiModelProperty(notes = "Generated Track-Name")
     String name;
-    @ApiModelProperty(notes = "GenerATED TRACK-Comment")
     String comment;
-
-
-    public Track() {
-    }
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    // Setters and Getters
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    @Override
-    public String toString() {
-        return "Track{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", comment='" + comment + '\'' +
-                '}';
-    }
 }
