d1e45583b5fa05542cd1e18cec9d2555dcae834f
srija <srijak890@gmail.com>
2019-08-07 10:18:42 +0530

    springbootmysql

---
+++ Pictures/BOOT/Spring-Boot-mysql-task/src/main/java/com/stackroute/muzix/MuzixApplication.java
@@ -0,0 +1,14 @@
+package com.stackroute.muzix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//spring boot main class
+@SpringBootApplication
+public class MuzixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuzixApplication.class, args);
+	}
+
+}
